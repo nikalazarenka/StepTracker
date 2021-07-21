03ac6fe3befcf91fbcb30ce55b973e62f0c9d472
Nicole Vy <nikavuskvarko@mail.ru>
2021-07-21 10:59:59 +0300

    Add project files.

---
+++ Data/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepTracker.Data
+{
+    public class Person
+    {
+        public int Rank { get; set; }
+        public string User { get; set; }
+
+
+    }
+}
