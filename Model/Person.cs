1daacb8623e325bc01a75609cd0108bf74694014
Nicole Vy <nikavuskvarko@mail.ru>
2021-07-21 12:55:23 +0300

    Refactoring Add Model Person.cs Add IOFileService.cs Add TestData Added display of user and steps in the mainwindow

---
+++ Model/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using StepTracker.Services;
+
+namespace StepTracker.Model
+{
+    public class Person
+    {
+        private static readonly string _path = $"{Environment.CurrentDirectory}\\TestData";
+        private static List<Person> _personsList = new List<Person>();
+        private static readonly List<Person> _allPersonsList = new List<Person>();
+        private static IOFileService _ioFileService;
+
+        public int Rank { get; set; }
+        public string User { get; set; }
+        public string Status { get; set; }
+        public int Steps { get; set; }
+
+        public static List<Person> GetPersons()
+        {
+            var numberOfFiles = new DirectoryInfo(_path).GetFiles().Length;
+
+            for (int i = 1; i <= numberOfFiles; i++)
+            {
+                _ioFileService = new IOFileService(_path + $"\\day{i}.json");
+
+                _personsList = _ioFileService.LoadData();
+                _allPersonsList.AddRange(_personsList);
+            }
+
+            return _allPersonsList;
+        }
+        
+
+    }
+}
---
+++ Services/IOFileService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using StepTracker.Model;
+
+namespace StepTracker.Services
+{
+    class IOFileService
+    {
+        
+
+        private readonly string _path;
+        public  IOFileService (string path)
+        {
+            _path = path;
+        }
+
+        public List<Person> LoadData()
+        {
+            var fileExists = File.Exists(_path);
+
+            if (!fileExists)
+            {
+                File.CreateText(_path).Dispose();
+                return new List<Person>();
+            }
+            using (var reader = File.OpenText(_path))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<List<Person>>(fileText);
+            }
+        }
+
+    }
+}
